7d901d7baf181e0ebe2338ef44d72215d5de1ec8
Unknown <306489696@qq.com>
2018-10-29 14:01:58 -0700

    add switch
    添加开关

---
+++ Assets/scripts/Switch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+void OnTriggerEnter (Collider other)
+{
+    Debug.Log("switch on");
+}
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("stay in");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exit");    
+    }
+}
